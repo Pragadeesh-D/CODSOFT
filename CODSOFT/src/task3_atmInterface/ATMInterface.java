a0c31cf22c0e1ccca930cf2181f0829b0be52f24
Pragadeesh <pragadeeshd07@gmail.com>
2025-07-24 01:16:58 +0530

    Add Task 3: ATM Interface

--- CODSOFT/src/task3_atmInterface/ATMInterface.java
+++ CODSOFT/src/task3_atmInterface/ATMInterface.java
@@ -1,13 +1,17 @@
 package task3_atmInterface;
 
 import java.util.Scanner;
+
+// This class manages the user's bank balance
 class UserBankAccount{
     private double balance = 0.0;
 
+    // Returns the current balance
     public double getBalance(){
         return balance;
     }
 
+    // Updates the balance to a new value
     public void setBalance(double balance){
         this.balance = balance;
     }
@@ -16,6 +20,8 @@ class UserBankAccount{
 public class ATMInterface {
     static Scanner sc = new Scanner(System.in);
     static double amount= 0.0;
+
+    // Handles withdrawing money from the account
     public static void withdraw(UserBankAccount account){
         while (true) {
             System.out.println("Enter amount to withdraw: ");
@@ -45,6 +51,7 @@ public class ATMInterface {
         userChoices();
     }
 
+    // Handles depositing money into the account
     public static void deposit(UserBankAccount account){
         while (true) {
             System.out.println("Enter amount to deposit: ");
@@ -69,11 +76,13 @@ public class ATMInterface {
         userChoices();
     }
 
+    // Displays the current balance in the user's account
     public static void checkBalance(UserBankAccount account){
         System.out.printf("Your current balance is %.2f \n", account.getBalance());
         userChoices();
     }
 
+    // Shows the list of available actions to the user
     public static void userChoices(){
         System.out.println("\nPlease choose an option:\n" +
                 "1. Withdraw\n" +
@@ -83,6 +92,7 @@ public class ATMInterface {
                 "Enter your choice: ");
     }
 
+    // Main method: Starts the ATM simulation and handles user interaction
     public static void main(String[] args){
         UserBankAccount account = new UserBankAccount();
         System.out.println("Hey User! Welcome to your ATM interface.\nYou can withdraw, deposit, and check your balance here.");
@@ -105,7 +115,7 @@ public class ATMInterface {
                         System.out.println("Thank you for using our ATM service! Have a great day!");
                         return;
                     default:
-                        System.out.println("Invalid ihoice! Please enter a valid option: ");
+                        System.out.println("Invalid choice! Please enter a valid option: ");
                 }
             }
             else {
